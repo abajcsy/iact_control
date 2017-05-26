fd8a9f77387301cb6a7911550661a59dfa9c252d
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2017-05-26 10:00:23 -0700

    updates to test_dist

--- src/misc_util/test_dist.py
+++ src/misc_util/test_dist.py
@@ -38,40 +38,77 @@ def robotToCartesian(robot):
 
 	return cartesian
 
-def create_table(env):
+def plotTable(env):
+	"""
+	Plots the robot table in OpenRAVE.
+	"""
 	# load table into environment
 	objects_path = find_in_workspaces(
-		    project='interactpy',
-		    path='envdata',
-		    first_match_only=True)[0]
+			project='interactpy',
+			path='envdata',
+			first_match_only=True)[0]
 	env.Load('{:s}/table.xml'.format(objects_path))
 	table = env.GetKinBody('table')
-	table.SetTransform(np.array([[1.0, 0.0,  0.0, 0],
-		                         [0.0, 1.0,  0.0, 0],
-		                         [0.0, 0.0,  1.0, -0.832],
-		                         [0.0, 0.0,  0.0, 1.0]]))
+	table.SetTransform(np.array([[0.0, 1.0,  0.0, 0],
+  								 [1.0, 0.0,  0.0, 0],
+			                     [0.0, 0.0,  1.0, -0.832],
+			                     [0.0, 0.0,  0.0, 1.0]]))
 	color = np.array([0.9, 0.75, 0.75])
 	table.GetLinks()[0].GetGeometries()[0].SetDiffuseColor(color)
 
-def create_table_mount(env, bodies):
+def plotTableMount(env,bodies):
+	"""
+	Plots the robot table mount in OpenRAVE.
+	"""
 	# create robot base attachment
 	body = RaveCreateKinBody(env, '')
 	body.InitFromBoxes(np.array([[0,0,0, 0.14605,0.4001,0.03175]]))
 	body.SetTransform(np.array([[1.0, 0.0,  0.0, 0],
-		                         [0.0, 1.0,  0.0, 0],
-		                         [0.0, 0.0,  1.0, -0.032],
-		                         [0.0, 0.0,  0.0, 1.0]]))
+			                     [0.0, 1.0,  0.0, 0],
+			                     [0.0, 0.0,  1.0, -0.032],
+			                     [0.0, 0.0,  0.0, 1.0]]))
 	body.SetName("robot_mount")
 	env.Add(body, True)
 	color = np.array([0.9, 0.58, 0])
 	body.GetLinks()[0].GetGeometries()[0].SetDiffuseColor(color)
 	bodies.append(body)
 
-env, robot = interact.initialize_empty('jaco_dynamics', empty=True)
+def plotLaptop(env,bodies):
+	"""
+	Plots the robot table mount in OpenRAVE.
+	"""
+	# create robot base attachment
+	body = RaveCreateKinBody(env, '')
+	#12 x 9 x 1 in, 0.3048 x 0.2286 x 0.0254 m
+	# divide by 2: 0.1524 x 0.1143 x 0.0127
+	#20 in from robot base
+	body.InitFromBoxes(np.array([[0,0,0,0.1143,0.1524,0.0127]]))
+	body.SetTransform(np.array([[1.0, 0.0,  0.0, -0.508],
+			                     [0.0, 1.0,  0.0, 0],
+			                     [0.0, 0.0,  1.0, -0.032],
+			                     [0.0, 0.0,  0.0, 1.0]]))
+	body.SetName("laptop")
+	env.Add(body, True)
+	color = np.array([0, 0, 0])
+	body.GetLinks()[0].GetGeometries()[0].SetDiffuseColor(color)
+	bodies.append(body)
+
+env, robot = interact.initialize_empty('jaco_dynamics', envXML='environ.env.xml', empty=True)
 bodies = []
 
-create_table(env)
-create_table_mount(env,bodies)
+viewer = env.GetViewer()
+viewer.SetSize(1000,1000)
+#viewer.SetCamera([
+#	[0.,  0., -1., 0.462949],
+#	[1.,  0.,  0., 2.192697],
+#	[0., -1.,  1., 0.936772],
+#	[0.,  0.,  0., 1.]])
+#viewer.SetBkgndColor([0.8,0.8,0.8])
+
+
+plotTable(env)
+plotTableMount(env,bodies)
+plotLaptop(env,bodies)
 
 cartesianDOFs = robotToCartesian(robot)
 #for coord in cartesianDOFs:
